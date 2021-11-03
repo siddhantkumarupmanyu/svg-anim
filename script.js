89ee2a89fb9d96d276ff2689ae1520d4002abed7
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-11-03 10:49:44 +0530

    page 1 done

--- script.js
+++ script.js
@@ -2,41 +2,41 @@ let butterflyContainer = document.querySelector(".butterfly-container")
 let butterfly = document.querySelector(".butterfly")
 let flowerCount = 0
 
-let button = document.querySelector("button")
-button.addEventListener("click", () => {
-    
+let flowerNext = document.querySelector(".flower-next")
+flowerNext.addEventListener("click", () => {
     document.querySelector("#page2").scrollIntoView()
+    // todo: reset the butterfly
 })
 
-// butterflyContainer.addEventListener("animationend", () => {
-//     butterfly.classList.remove("butterfly-animate")
+butterflyContainer.addEventListener("animationend", () => {
+    butterfly.classList.remove("butterfly-animate")
 
-//     flowerCount++
+    flowerCount++
 
-//     setTimeout(() => {
-//         butterflyContainer.classList.remove("animate-path")
+    if (flowerCount == 3) {
+        flowerNext.style.visibility = "visible"
+        return
+    }
 
-//         if (flowerCount == 1) { // reached at first flower
-//             butterflyContainer.classList.remove("first-path")
-//             butterflyContainer.classList.add("second-path")
-//         }
-//         else if (flowerCount == 2) {
-//             butterflyContainer.classList.remove("second-path")
-//             butterflyContainer.classList.add("third-path")
-//         }
+    setTimeout(() => {
+        butterflyContainer.classList.remove("animate-path")
 
-//         else if (flowerCount == 3) {
-//             butterflyContainer.classList.add("animate-path")
-//             return
-//         }
+        if (flowerCount == 1) { // reached at first flower
+            butterflyContainer.classList.remove("first-path")
+            butterflyContainer.classList.add("second-path")
+        }
+        else if (flowerCount == 2) {
+            butterflyContainer.classList.remove("second-path")
+            butterflyContainer.classList.add("third-path")
+        }
 
-//         butterfly.classList.add("butterfly-animate")
-//         setTimeout(() => {
-//             butterflyContainer.classList.add("animate-path")
-//         }, 50)
-//     }, 1000)
-// })
+        butterfly.classList.add("butterfly-animate")
+        setTimeout(() => {
+            butterflyContainer.classList.add("animate-path")
+        }, 50)
+    }, 1000)
+})
 
-// window.addEventListener('load', function () {
-//     butterflyContainer.classList.add("animate-path")
-// })
+window.addEventListener('load', function () {
+    butterflyContainer.classList.add("animate-path")
+})
